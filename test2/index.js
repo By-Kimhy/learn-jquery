313da7d0fe8623dd99f51e36d581a8002b398f4c
Kim hy <kimhyby99@gmail.com>
2026-07-06 00:54:19 +0700

    parent parents find children eq

--- test2/index.js
+++ test2/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var body=$('body');
     var i=0;
+    // prepend() append() before() after()
     $("#btn1").click(function(){
         i++;
         var box= '<div class="box">'+i+'</div>';
@@ -9,8 +10,36 @@ $(document).ready(function(){
         $('.box2').before(box);
         // $('.box2').after(box);
     })
+    // remove() empty()
     $("#btnRemove").click(function(){
         // $('.box3').remove();
         $('.box3').empty();
     });
+
+    // addClass() 
+    $("#btnAddClass").click(function(){
+        $('.box2').addClass('box4');
+    });
+    //removeClass()
+    $("#btnRemoveClass").click(function(){
+        $('.box2').removeClass('box4');
+    });
+    // parent() parents()
+    $(".aa").click(function(){
+        // $(this).parent().css('background-color','black');
+        $(this).parents('.box10').css('background-color','green');
+    })
+
+    // find and children use in the same way but the difference is 
+    // find() will search for all the children and grandchildren of the selected element, 
+    // while children() will only search for the immediate children of the selected element.
+    
+    // find() eq()
+    $("#aa").click(function(){
+        $(this).parents('.item-box').find('.box10').eq(0).css('background-color','grey');
+    })
+    // children() eq()
+    $("#bb").click(function(){
+        $(this).parents('.item-box').children('.box10').eq(0).css('background-color','yellow');
+    })
 })
